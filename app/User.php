80e6f650dc0d537b907d10cebd67405da8445168
Artem <artemnegel@gmail.com>
2020-03-02 18:14:26 +0200

    add: models User Role and controller UserController

--- app/User.php
+++ app/User.php
@@ -36,4 +36,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles(){
+        return $this->belongsToMany(Models\Role::class);
+    }
+
+    public function hasRole(string $curRole){
+        foreach ($this->roles as $role) {
+            if($role->name == $curRole)
+                return true;
+        }
+        return false;
+    }
+
+    public function hasAnyRole(array $roles){
+        if($this->roles()->whereIn('name', $roles)->first()){
+            return true;
+        }
+        return false;
+    }
 }
